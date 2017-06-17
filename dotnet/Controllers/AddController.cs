0dfa5bf6099c3c6119be0fc27e01e6f8b10b7f54
Luke McGregor <luke@mcgregor.org.nz>
2017-06-17 23:53:03 +1200

    Added dot net project

---
+++ dotnet/Controllers/AddController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basic_api_dotnet.Controllers
+{
+    public class AddController : Controller
+    {
+        [HttpGet]
+        [Route("add/{x}/to/{y}")]
+        public IActionResult Get(int x, int y)
+        {
+            return Ok(x + y);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Post([FromBody]int[] numbers)
+        {
+            return Ok(new { sum = numbers.Sum() });
+        }
+    }
+}
