338e46dced7e7bfb578c9dcc6d4ae67656cebaa0
Luke McGregor <lukemcgregor@users.noreply.github.com>
2017-06-20 14:12:13 +1200

    Working nodejs sample

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,15 +1,30 @@
 var express    = require('express');
 var app        = express();
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+var xml = require('xml');
 
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+require('body-parser-xml')(bodyParser);
+
+app.use(bodyParser.json());
+app.use(bodyParser.xml());
 
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/add/:x/to/:y', function(req, res) {
     res.json({ sum: parseInt(req.params.x) + parseInt(req.params.y) });
+});
+
+router.post('/add', function(req, res) {
+    var sum = req.body.numbers.value.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    res.format({
+        xml: () => {
+            res.send(xml({sum:sum}));
+        },
+        json: () => {
+            res.json({sum:sum});
+        }
+    })
 });
 
 app.use('/', router);
