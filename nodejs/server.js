c660fb2b6129d47147fb8465f2219df0dd3ce043
Thomas Coats <github@voodoolabs.net>
2017-06-28 19:39:34 +1200

    Stylistic changes

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,29 +1,21 @@
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
-var xml = require('xml');
+var app = require('express')()
+var xml = require('xml')
 
-require('body-parser-xml')(bodyParser);
-
-app.use(bodyParser.json());
-app.use(bodyParser.xml());
-
-var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser')
+require('body-parser-xml')(bodyParser)
+app.use(bodyParser.json())
+app.use(bodyParser.xml())
 
 app.get('/add/:x/to/:y', (req, res) => {
-    res.json({ sum: parseInt(req.params.x) + parseInt(req.params.y) });
-});
+    res.json({ sum: parseInt(req.params.x) + parseInt(req.params.y) })
+})
 
 app.post('/add', (req, res) => {
-    var sum = req.body.numbers.value.map(parseInt).reduce((a, b) => a + b, 0);
+    var sum = req.body.numbers.value.map(parseInt).reduce((a, b) => a + b)
     res.format({
-        xml: () => {
-            res.send(xml({sum:sum}));
-        },
-        json: () => {
-            res.json({sum:sum});
-        }
+        xml: () => res.send(xml({sum:sum})),
+        json: () => res.json({sum:sum})
     })
-});
+})
 
-app.listen(port);
+app.listen(8080)
